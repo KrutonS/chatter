19cceb6bf70ce8abdcc4dbab8a974528cec42d61
KrutonS <krutonpl@gmail.com>
2022-01-28 23:27:04 +0100

    Typography file organize

--- components/common/typography.tsx
+++ components/common/typography.tsx
@@ -3,23 +3,30 @@ import { Text, TextStyle } from "react-native";
 import { typoStyles } from "../../styles";
 import { capitalize } from "../../utils/string";
 
+//#region Local
 interface CommonProps {
   style?: TextStyle;
   children: string;
 }
+
+// morph typoStyles into object with components as recors
+
 const typoEntries = Object.entries<TextStyle>(typoStyles);
 type TypoKeys = keyof typeof typoStyles;
 type NewKeys = Capitalize<TypoKeys>;
 
-const typography = typoEntries.reduce((obj, [key, typoStyle]) => {
+const typography = typoEntries.reduce((typoObj, [key, typoStyle]) => {
   const newKey = capitalize(key);
   const Component = ({ children, style }: CommonProps) => (
     <Text style={{ ...typoStyle, ...style }}>{children}</Text>
   );
-  return { ...obj, [newKey]: Component };
+  return { ...typoObj, [newKey]: Component };
 }, {} as Record<NewKeys, FC<CommonProps>>);
 
 type TypoProps = { type: TypoKeys } & CommonProps;
+//#endregion
+
+// 👌🎉🎉🎉
 const Typography = ({ type, children, style }: TypoProps) => {
   const SelectedTypo = typography[capitalize(type)];
 
