e7344834528fc600749cd527bd891e66de47bad7
KrutonS <krutonpl@gmail.com>
2022-01-29 00:57:06 +0100

    refactor redundant code

--- components/common/typography.tsx
+++ components/common/typography.tsx
@@ -1,36 +1,19 @@
-import { FC } from "react";
 import { Text, TextStyle } from "react-native";
 import { typoStyles } from "../../styles";
-import { capitalize } from "../../utils/string";
 
-//#region Local
 interface CommonProps {
   style?: TextStyle;
   children: string;
 }
 
-// morph typoStyles into object with components as recors
-
-const typoEntries = Object.entries<TextStyle>(typoStyles);
 type TypoKeys = keyof typeof typoStyles;
-type NewKeys = Capitalize<TypoKeys>;
-
-const typography = typoEntries.reduce((typoObj, [key, typoStyle]) => {
-  const newKey = capitalize(key);
-  const Component = ({ children, style }: CommonProps) => (
-    <Text style={{ ...typoStyle, ...style }}>{children}</Text>
-  );
-  return { ...typoObj, [newKey]: Component };
-}, {} as Record<NewKeys, FC<CommonProps>>);
-
 type TypoProps = { type: TypoKeys } & CommonProps;
-//#endregion
 
-// 👌🎉🎉🎉
 const Typography = ({ type, children, style }: TypoProps) => {
-  const SelectedTypo = typography[capitalize(type)];
-
-  return <SelectedTypo style={style}>{children}</SelectedTypo>;
+  const Typo = ({ children, style }: CommonProps) => (
+    <Text style={{ ...typoStyles[type], ...style }}>{children}</Text>
+  );
+  return <Typo style={style}>{children}</Typo>;
 };
 
 export default Typography;
